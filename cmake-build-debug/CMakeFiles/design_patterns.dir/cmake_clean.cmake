4aa0594fb2fa3ec54e0f3d5f4457d3c0984ae10e
sajith <sajith@digitalxlabs.com>
2020-10-13 20:23:00 +0530

    Init

---
+++ cmake-build-debug/CMakeFiles/design_patterns.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design_patterns.dir/main.cpp.o"
+  "design_patterns.pdb"
+  "design_patterns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design_patterns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
