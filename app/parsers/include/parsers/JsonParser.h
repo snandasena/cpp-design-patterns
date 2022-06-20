81fcc512feb7b71a0c27f36ec600377e292aab6c
snandasena <sajithgithub@gmail.com>
2022-06-20 23:03:45 +0530

    Imoproved

--- app/parsers/include/parsers/JsonParser.h
+++ app/parsers/include/parsers/JsonParser.h
@@ -5,4 +5,18 @@
 #ifndef DESIGN_PATTERNS_JSONPARSER_H
 #define DESIGN_PATTERNS_JSONPARSER_H
 
+#include "parsers/ICredentialsParser.h"
+#include "parsers/IReportParser.h"
+
+namespace parsers
+{
+    class JsonParser : public IReportParser, public ICredentialParser
+    {
+    public:
+        std::optional <types::Report> parseReport(const std::string &) const override;
+
+        std::optional <types::User> parseCredentials(const std::string &) const override;
+    };
+}
+
 #endif //DESIGN_PATTERNS_JSONPARSER_H
--- app/parsers/src/JsonParser.cpp
+++ app/parsers/src/JsonParser.cpp
@@ -2,3 +2,5 @@
 // Created by sajith on 6/20/22.
 //
 
+#include "parsers/JsonParser.h"
+
