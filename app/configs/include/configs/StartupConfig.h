595035fb59752b128dbda148b8435ce852f22f7e
snandasena <sajithgithub@gmail.com>
2022-06-23 12:31:25 +0530

    Imoproved

--- app/configs/include/configs/StartupConfig.h
+++ app/configs/include/configs/StartupConfig.h
@@ -5,4 +5,19 @@
 #ifndef DESIGN_PATTERNS_STARTUPCONFIG_H
 #define DESIGN_PATTERNS_STARTUPCONFIG_H
 
+
+#include <cstdint>
+#include <optional>
+
+#include "types/ReportFormat.h"
+
+struct StartupConfig
+{
+    const std::uint16_t port;
+    const types::ReportFormat format;
+};
+
+std::optional<const StartupConfig> optionsToStartupConfig(int arg, char *argv[]);
+
+
 #endif //DESIGN_PATTERNS_STARTUPCONFIG_H
