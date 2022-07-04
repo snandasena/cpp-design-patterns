aee8eae0c35db65b01669b0f8d0d959981764894
Sajith Nandasena <sajithgithub@gmail.com>
2022-07-04 12:14:25 +0530

    Improved

--- app/servers/include/servers/Server.h
+++ app/servers/include/servers/Server.h
@@ -5,4 +5,21 @@
 #ifndef DESIGN_PATTERNS_SERVER_H
 #define DESIGN_PATTERNS_SERVER_H
 
+
+#include <cstdint>
+#include <memory>
+
+#include "auth/IAuthentication.h"
+#include "parsers/ICredentialsParser.h"
+#include "services/ITaxServiceFactory.h"
+
+namespace servers
+{
+    using Port = std::uint16_t;
+
+    void runServer(Port port, const auth::IAuthentication &, const parsers::ICredentialsParser &,
+                   const services::ITaxServiceFactory &);
+}
+
+
 #endif //DESIGN_PATTERNS_SERVER_H
--- app/servers/src/Server.cpp
+++ app/servers/src/Server.cpp
@@ -1,3 +1,31 @@
 //
 // Created by sajith on 6/28/22.
 //
+#include "servers/Server.h"
+
+#include <boost/asio.hpp>
+#include <memory>
+#include <thread>
+
+#include "servers/ReportSession.h"
+
+
+using boost::asio::ip::tcp;
+
+namespace servers
+{
+    void runServer(Port port, const auth::IAuthentication &authManager,
+                   const parsers::ICredentialsParser &credentialsParser,
+                   const services::ITaxServiceFactory &taxServiceFactory)
+    {
+        boost::asio::io_context io_context;
+        tcp::acceptor a(io_context, tcp::endpoint(tcp::v4(), port));
+
+        for (;;)
+        {
+            std::thread(&ReportSession::start,
+                     ReportSession(a.accept(), authManager, credentialsParser, taxServiceFactory))
+                    .detach();
+        }
+    }
+}
